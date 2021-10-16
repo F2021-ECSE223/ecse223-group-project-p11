942f40588df8b11ee4cec66a3f7928cc66e0498a
anaelledrai <83705526+anaelledrai@users.noreply.github.com>
2021-10-16 12:19:22 -0400

    Controller feature set 3 first version

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet3Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet3Controller.java
@@ -1,11 +1,38 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+import ca.mcgill.ecse.climbsafe.model.Guide;
+
 public class ClimbSafeFeatureSet3Controller {
 
-  public static void registerGuide(String email, String password, String name,
-      String emergencyContact) throws InvalidInputException {}
+	public static void registerGuide(String email, String password, String name, String emergencyContact)
+			throws InvalidInputException {
+		String error = "";
+		ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
+		try {
+			climbSafe.addGuide(email, password, name, emergencyContact);
+		} catch (RuntimeException e) {
+			error = e.getMessage();
+			if (error.equals(
+					"Cannot create due to duplicate number. See http://manual.umple.org?RE003ViolationofUniqueness.html")) {
+				error = "An account with this email already exists. Please use a different email.";
+			}
+			throw new InvalidInputException(error);
+		}
+	}
 
-  public static void updateGuide(String email, String newPassword, String newName,
-      String newEmergencyContact) throws InvalidInputException {}
-
+	public static void updateGuide(String email, String newPassword, String newName, String newEmergencyContact)
+			throws InvalidInputException {
+		String error = "";
+		ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
+		try {
+			Guide guideToUpdate = (Guide) Guide.getWithEmail(email);
+			guideToUpdate.setPassword(newPassword);
+			guideToUpdate.setName(newName);
+			guideToUpdate.setEmergencyContact(newEmergencyContact);
+		} catch (RuntimeException e) {
+			throw new InvalidInputException(e.getMessage());
+		}
+	}
 }
