ff4e3e41a46c67637c429c339c14202a647ca763
anaelledrai <83705526+anaelledrai@users.noreply.github.com>
2021-10-26 16:40:00 -0400

    Update ClimbSafeFeatureSet1Controller.java

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet1Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet1Controller.java
@@ -9,13 +9,12 @@ import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
 import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
 import ca.mcgill.ecse.climbsafe.model.Guide;
 import ca.mcgill.ecse.climbsafe.model.Member;
-
+import ca.mcgill.ecse.climbsafe.model.NamedUser;
 
 public class ClimbSafeFeatureSet1Controller {
 
-  public static void setup(Date startDate, int nrWeeks, int priceOfGuidePerWeek)
-      throws InvalidInputException {
-    String error  ="";
+	public static void setup(Date startDate, int nrWeeks, int priceOfGuidePerWeek) throws InvalidInputException {
+		String error = "";
 //    Calendar cal = Calendar.getInstance();
 //    cal.setLenient(false);
 //    cal.setTime(startDate);
@@ -25,35 +24,32 @@ public class ClimbSafeFeatureSet1Controller {
 //    catch (Exception e) {
 //      error = "Invalid Date";
 //    }
-    if (nrWeeks<0 || nrWeeks==0) {
-      error = "The number of climbing weeks must be greater than or equal to zero";
-    }
-    if (priceOfGuidePerWeek<0 || priceOfGuidePerWeek==0) {
-      error = "The price of guide per week must be greater than or equal to zero";
-    }
-    if (!error.isEmpty()) {
-      throw new InvalidInputException(error);}
-//    ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
-//    try {
-//      climbSafe.setStartDate(startDate);
-//  } catch (RuntimeException e) {
-//      error = e.getMessage();
-//      throw new InvalidInputException(error);}
-//    try {
-//      climbSafe.setPriceOfGuidePerWeek(priceOfGuidePerWeek);
-//    }catch(RuntimeException e) {
-//      error = e.getMessage();
-//      throw new InvalidInputException
-     
-    
-  }
+		// ClimbSafe climbsafe = ClimbSafeApplication.getClimbSafe();
+		if (nrWeeks < 0 || nrWeeks == 0) {
+			error = "The number of climbing weeks must be greater than or equal to zero";
+		}
+		if (priceOfGuidePerWeek < 0 || priceOfGuidePerWeek == 0) {
+			error = "The price of guide per week must be greater than or equal to zero";
+		}
+		if (!error.isEmpty()) {
+			throw new InvalidInputException(error);
+		}
 
-    
+		ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
+		try {
+			climbSafe.setStartDate(startDate);
+			climbSafe.setPriceOfGuidePerWeek(priceOfGuidePerWeek);
+			climbSafe.setNrWeeks(nrWeeks);
 
+		} catch (RuntimeException e) {
+			error = e.getMessage();
+			throw new InvalidInputException(error);
+		}
 
- 
-    //Date datee = Date.valueOf(("startDate"));
-    
+	}
+
+	// Date datee = Date.valueOf(("startDate"));
+
 //    String newdate = startDate.toString();
 //    char[] ch = new char[newdate.length()];
 //    
@@ -89,42 +85,47 @@ public class ClimbSafeFeatureSet1Controller {
 //    if ((u==0 && p==0)){
 //      error = "Invalid Date";}
 
+	// if (.isValid()==false) {
+	// error = "Invalid date";
 
-   
-    
-    
-    
-    //if (.isValid()==false) {
-      //error = "Invalid date";
-      
-    //}
-  
-  //.isValid()
-    //String strdate = startDate.toString();
-    //if (strdate.getNumericalValueOf(strdate.charAt(6))>1) {
-  //}
+	// }
 
-  public static void deleteMember(String email) throws InvalidInputException {
-    try {
-      Member memberToDelete = (Member) Member.getWithEmail(email);
-      memberToDelete.delete();}
-    catch (RuntimeException e) {
-      throw new InvalidInputException(e.getMessage());
-    }}
-    
-    
-   
- 
-  
+	// .isValid()
+	// String strdate = startDate.toString();
+	// if (strdate.getNumericalValueOf(strdate.charAt(6))>1) {
+	// }
 
-  public static void deleteGuide(String email) throws InvalidInputException {
-    try {
-    Guide guideToDelete = (Guide) Guide.getWithEmail(email);
-    guideToDelete.delete();}
-    catch (RuntimeException e) {
-      throw new InvalidInputException(e.getMessage());
-    }}
-  // this method needs to be implemented only by teams with seven team members
-  public static void deleteHotel(String name) {}
+	public static void deleteMember(String email) {
+		String error = "";
+		if (!NamedUser.hasWithEmail(email) || !(NamedUser.getWithEmail(email) instanceof Member)) {
+//		error = "The member does not exist in the sytem";
+		} else {
+//			try {
+				Member memberToDelete = (Member) Member.getWithEmail(email);
+				memberToDelete.delete();
+//			} catch (RuntimeException e) {
+//				throw new RuntimeException(e.getMessage());
+//			}
+		}
+
+	}
+
+	public static void deleteGuide(String email) {
+		String error = "";
+		if (!NamedUser.hasWithEmail(email) || !(NamedUser.getWithEmail(email) instanceof Guide)) {
+//		error = "The member does not exist in the sytem";
+		} else {
+//			try {
+				Guide memberToDelete = (Guide) Guide.getWithEmail(email);
+				memberToDelete.delete();
+//			} catch (RuntimeException e) {
+//				throw new RuntimeException(e.getMessage());
+//			}
+		}
+	}
+
+	// this method needs to be implemented only by teams with seven team members
+	public static void deleteHotel(String name) {
+	}
 
 }
