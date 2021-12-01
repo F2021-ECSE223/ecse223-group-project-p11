a9d1e4f281e214792a06af5fa4f0c29dde82ac85
Toad234 <oliver.cafferty@mail.mcgill.ca>
2021-11-30 23:14:59 -0500

    Update ViewAssignmentFrame.java

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/ViewAssignmentFrame.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/view/ViewAssignmentFrame.java
@@ -19,61 +19,90 @@ import ca.mcgill.ecse.climbsafe.controller.InvalidInputException;
 import ca.mcgill.ecse.climbsafe.controller.TOEquipment;
 import ca.mcgill.ecse.climbsafe.controller.AddtitionalController;
 
-
-
 import java.sql.*;
 import java.time.*;
 
 import java.awt.event.ActionEvent;
+private JComboBox<String> memberEmails=new JComboBox<>();
+for(String email: AddtitionalController.getAssignmentMemberEmails()) {
+	memberEmails.addItem(email);
+}
+private JComboBox<String> memberNames=new JComboBox<>();
+for(String names: AddtitionalController.getAssignmentMemberNames()) {
+	memberNames.addItem(names);
+}
+private JComboBox<String> guideEmails=new JComboBox<>();
+for(String email: AddtitionalController.getAssignmentGuideEmails()) {
+	guideEmails.addItem(email);
+}
+private JComboBox<String> guideNames=new JComboBox<>();
+for(String names: AddtitionalController.getAssignmentGuideNames()) {
+	guideNames.addItem(names);
+}
+private JComboBox<String> hotelNames=new JComboBox<>();
+for(String names: AddtitionalController.getAssignmentHotelNames()) {
+	hotelNames.addItem(names);
+}
+private JComboBox<Integer> startWeeks = new JComboBox<>();
+for(Integer num: AddtitionalController.getAssignmentStartWeeks()) {
+	startWeeks.addItem(num);
+}
+private JComboBox<Integer> endWeeks = new JComboBox<>();
+for(Integer num: AddtitionalController.getAssignmentEndWeeks()) {
+	endWeeks.addItem(num);
+}
+private JComboBox<Integer> totalGuideCosts = new JComboBox<>();
+for(Integer num:AddtitionalController.getAssignmentTotalGuideCosts()) {
+	totalGuideCosts.addItem(num);
+}
+private JComboBox<Integer> totalEquipmentCosts = new JComboBox<>();
+for(Integer num:AddtitionalController.getAssignmentTotalEquipmentCosts()) {
+	totalEquipmentCosts.addItem(num);
+}
+private JComboBox<String> statuses=new JComboBox<>();
+for(String names: AddtitionalController.getAssignmentStatuses()) {
+	statuses.addItem(names);
+}
 
 public class ViewAssignmentFrame extends JFrame {
-	List<String> memberEmails= new ArrayList<String>();
-	List<String> guideEmails= new ArrayList<String>();
-	List<String> equipment= new ArrayList<String>();
-	List<Integer> equipmentNumber= new ArrayList<Integer>();
-	List<String> bundles= new ArrayList<String>();
-	List<Integer> bundleNumber= new ArrayList<Integer>();
 	
+
 	private static final long serialVersionUID = 99L;
+
 	
 	
-	private JComboBox<String> memberEmailsBox= new JComboBox<>();
-	private JComboBox<String> guideEmailsBox= new JComboBox<>();
-	
-	private JTable assignmentOverview= new JTable(new DefaultTableModel()){
-		private static final long serialVersionUID=99L;
+
+	private JTable assignmentOverview = new JTable(new DefaultTableModel()) {
+		private static final long serialVersionUID = 99L;
 	};
-	/*var assignment= new DefaultTableModel(0,0);
-	assignment.setColumnIdentifiers(ASSIGNMENT_COLUMN_NAMES);*/
-	  private JScrollPane overviewScrollPane = new JScrollPane(assignmentOverview);
-	  //maybe remove status below
-	  private static final String[] ASSIGNMENT_COLUMN_NAMES = {"memberEmail","memberName", "guideEmail","guideName", "hotelName", "startWeek","endWeek", "totalCostForGuide", "totalCostForEquipment","Status" };
-	  private static final int HEIGHT_OVERVIEW_TABLE = 200;
-	  
-	  public ViewAssignmentFrame() {//maybe
-		  initComponents();
-	  }
-	  private void initComponents() {
+	/*
+	 * var assignment= new DefaultTableModel(0,0);
+	 * assignment.setColumnIdentifiers(ASSIGNMENT_COLUMN_NAMES);
+	 */
+	private JScrollPane overviewScrollPane = new JScrollPane(assignmentOverview);
+	// maybe remove status below
+	private static final String[] ASSIGNMENT_COLUMN_NAMES = { "memberEmail", "memberName", "guideEmail", "guideName",
+			"hotelName", "startWeek", "endWeek", "totalCostForGuide", "totalCostForEquipment", "Status" };
+	private static final int HEIGHT_OVERVIEW_TABLE = 200;
+
+	public ViewAssignmentFrame() {// maybe
+		initComponents();
+	}
+	
+	private void makeTable() {
+		
+	}
+	private void initComponents() {
 		  add(overviewScrollPane);
 		    overviewScrollPane.setPreferredSize(new Dimension(assignmentOverview.getPreferredSize().width, HEIGHT_OVERVIEW_TABLE));
 		    overviewScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		    
 		    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 			setTitle("ClimbSafe Application System");
-			//fix
-			List<String> memberEmails = AddtitionalController.getAssignmentMemberEmails();
-			List<String> memberNames= AddtitionalController.getAssignmentMemberNames();
-			List<String> guideEmails = AddtitionalController.getAssignmentGuideEmails();
-			List<String> gudieNames= AddtitionalController.getAssignmentGuideNames();
-			List<String> guideNames= AddtitionalController.getAssignmentGuideNames();
-			List<String> hotelNames= AddtitionalController.getAssignmentHotelNames();
-			List<Integer> startWeeks=AddtitionalController.getAssignmentStartWeeks();
-			List<Integer> endWeeks=AddtitionalController.getAssignmentEndWeeks();
-			List<Integer> totalGuideCosts=AddtitionalController.getAssignmentTotalGuideCosts();
-			List<Integer> totalEquipmentCosts=AddtitionalController.getAssignmentTotalEquipmentCosts();
-			List<String> statuses= AddtitionalController.getAssignmentStatuses();	
-			
+			//set up comboboxes
+		
 			
 					
-			
+	
 	  }
 }
